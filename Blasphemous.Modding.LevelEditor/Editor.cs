910332a403f44feaabee0707681f12a41e3ee696
unknown <brandenek03912@hotmail.com>
2024-02-09 09:50:26 -0500

    import: load levels separetely

--- Blasphemous.Modding.LevelEditor/Editor.cs
+++ Blasphemous.Modding.LevelEditor/Editor.cs
@@ -1,12 +1,9 @@
 using Blasphemous.Modding.LevelEditor.Components;
-using Blasphemous.Modding.LevelEditor.Framework;
+using Blasphemous.Modding.LevelEditor.Import;
 using Blasphemous.Modding.LevelEditor.UI;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Blasphemous.Modding.LevelEditor;
@@ -30,36 +27,6 @@ public partial class Editor : Form
     public WindowGrid CreateGridWindow() => new(grid_contents, info_text);
 
     public WindowTool CreateToolWindow() => new();
-
-    // Json loading
-
-    // Move this to an importer that the grid has ?
-    private void LoadLevelObjects()
-    {
-        List<Thing> objects = new();
-
-        foreach (var group in LoadObjects().GroupBy(x => x.sprite.layer).OrderBy(x => x.Key))
-        {
-            foreach (var obj in group.OrderBy(x => x.sprite.order))
-            {
-                objects.Add(Thing.Import(obj));
-            }
-        }
-
-        Core.Grid.LoadLevel(objects);
-    }
-
-    private IEnumerable<ThingImport> LoadObjects()
-    {
-        string path = Path.Combine(Environment.CurrentDirectory, "import", "levels", SCENE + ".json");
-        Logger.Warning($"Importing level data from {path}");
-
-        if (!File.Exists(path))
-            return Array.Empty<ThingImport>();
-
-        string json = File.ReadAllText(path);
-        return JsonConvert.DeserializeObject<ThingImport[]>(json) ?? Array.Empty<ThingImport>();
-    }
 
     // Rescaling
 
@@ -87,6 +54,8 @@ public partial class Editor : Form
 
     private void OnClickLoad(object sender, EventArgs e)
     {
-        LoadLevelObjects();
+        Logger.Info($"Loading level {SCENE}");
+        IEnumerable<Thing> objects = LevelImporter.Load(SCENE);
+        Core.Grid.LoadLevel(objects);
     }
 }
---
+++ Blasphemous.Modding.LevelEditor/Import/LevelImporter.cs
@@ -0,0 +1,28 @@
+﻿using Blasphemous.Modding.LevelEditor.Components;
+using Blasphemous.Modding.LevelEditor.Framework;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Blasphemous.Modding.LevelEditor.Import;
+
+public static class LevelImporter
+{
+    public static IEnumerable<Thing> Load(string level)
+    {
+        return LoadInternal(level).Select(x => Thing.Import(x));
+    }
+
+    private static IEnumerable<ThingImport> LoadInternal(string level)
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "import", "levels", $"{level}.json");
+
+        if (!File.Exists(path))
+            return Array.Empty<ThingImport>();
+
+        string json = File.ReadAllText(path);
+        return JsonConvert.DeserializeObject<ThingImport[]>(json) ?? Array.Empty<ThingImport>();
+    }
+}
